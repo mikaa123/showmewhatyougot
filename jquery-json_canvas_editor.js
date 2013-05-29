13f1d8dbe07047face395579653d44a5d643d371
Michael Sokol <mikaa123@gmail.com>
2013-05-29 11:26:46 +0200

    design using purecss and some improvements

--- jquery-json_canvas_editor.js
+++ jquery-json_canvas_editor.js
@@ -15,9 +15,9 @@
   // ---------
 
   var selectedTool,
-    canvas,
     $this,
     stage,
+    layer,
     nodes = [];
 
   var textEditingView = {
@@ -39,8 +39,8 @@
 
     setModel: function(textNode) {
       this.model = textNode;
-      this.$font.val(this.model.getFontFamily());
-      this.$size.val(this.model.getFontSize());
+      this.$font.val(this.model && this.model.getFontFamily() || '');
+      this.$size.val(this.model && this.model.getFontSize() || '');
     }
   };
 
@@ -50,9 +50,9 @@
       this.$remove = $elt.find('#remove');
       this.$remove.on('click', function() {
         if (that.model) {
-          console.log('removing', that.model);
-          nodes.splice(nodes.indexOf(that.model), 1);
-          refreshCanvas();
+          that.model.remove();
+          freeEditionViews();
+          stage.draw();
         }
       });
     },
@@ -62,85 +62,26 @@
     }
   };
 
+  function freeEditionViews() {
+    editView.setModel(undefined);
+    textEditingView.setModel(undefined);
+  }
+
   // Support
   // -------
 
-  var Node = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.dragged = false;
-  };
-
-  Node.prototype = {
-    // returns true if there's a collision
-    collide: function(x, y) {
-      if (this.x <= x && this.y <= y &&
-          (this.x + 180) >= x && (this.y + 40) >= y) {
-        this.dragged = true;
-        this.draggedSource = { x: x, y: y};
-        editView.setModel(this);
-        this.collideAction();
-        return true;
-      }
-      return false;
-    },
-
-    moveTo: function(x, y) {
-      this.x += x - this.draggedSource.x;
-      this.y += y - this.draggedSource.y;
-      this.draggedSource.x = x;
-      this.draggedSource.y = y;
-    }
-  };
-
-  // var TextNode = function(x, y) {
-  //   Node.call(this, x, y);
-  //   this.text = "Placeholder";
-  //   this.style = CST.DEFAULT_FONT;
-  // };
-
-  // TextNode.prototype = Object.create(Node.prototype);
-
-  // TextNode.prototype.draw = function(context) {
-  //   context.save();
-  //   context.font = this.style;
-  //   context.fillText(this.text, this.x, this.y);
-  //   context.restore();
-  // };
-
-  // TextNode.prototype.collideAction = function() {
-  //   textEditingView.setModel(this);
-  // };
-
-  // var ImageNode = function(x, y, url) {
-  //   Node.call(this, x, y);
-  //   this.img = new Image();
-  //   this.img.src = url;
-  //   this.width = this.img.width;
-  //   this.height = this.img.height;
-  //   var that = this;
-  //   this.img.onload = function() {
-  //     that.draw(canvas.getContext('2d'));
-  //   };
-  // };
-
-  // ImageNode.prototype = Object.create(Node.prototype);
-
-  // ImageNode.prototype.draw = function(context) {
-  //     context.drawImage(this.img, this.x, this.y);
-  // };
-
-  // ImageNode.prototype.collideAction = function() {
-  //   // textEditingView.setModel(this);
-  // };
-
   var ImageNode = function(img, x, y) {
-    Kinetic.Image.call(this, {
+    Kinetic.Group.call(this, {
       x: x,
       y: y,
-      draggable: true,
-      image: img
+      draggable: true
     });
+    this.add(new Kinetic.Image({
+      x: 0,
+      y: 0,
+      name: 'image',
+      image: img
+    }));
 
     this
     .on('click', function(evt) {
@@ -148,7 +89,7 @@
     });
   };
 
-  ImageNode.prototype = Object.create(Kinetic.Image.prototype);
+  ImageNode.prototype = Object.create(Kinetic.Group.prototype);
 
   var TextNode = function(x, y) {
     Kinetic.Text.call(this, {
@@ -167,21 +108,23 @@
       stage.draw();
     })
     .on('click', function(evt) {
+      this.moveToTop();
       this.edit();
+    })
+    .on('mouseover', function(evt) {
+      document.body.style.cursor = 'pointer';
+    })
+    .on('mouseout', function() {
+        document.body.style.cursor = 'default';
     });
   };
 
   TextNode.prototype = Object.create(Kinetic.Text.prototype);
 
   TextNode.prototype.edit = function() {
+    editView.setModel(this);
     textEditingView.setModel(this);
   };
-
-  function refreshCanvas() {
-    canvas.width = canvas.width;
-    setCanvasStyle(canvas);
-    $.each(nodes, function() { this.draw(canvas.getContext('2d')); });
-  }
 
   function canvasToEditor() {
     stage = new Kinetic.Stage({
@@ -190,7 +133,7 @@
       height: 600
     });
 
-    var layer = new Kinetic.Layer();
+    layer = new Kinetic.Layer();
     stage.add(layer);
 
     $(stage.getContainer())
@@ -201,7 +144,11 @@
           nodes.push(text);
           layer.add(text);
           stage.draw();
-          selectedTool = CST.TOOLS.HAND;
+          toolSelection.selectHand();
+          text.edit();
+          break;
+        case CST.TOOLS.HAND:
+          freeEditionViews();
           break;
       }
     });
@@ -235,56 +182,32 @@
       }
       evt.preventDefault();
     }, false);
-
-    // $(canvas)
-    // .on('mousedown', function(evt) {
-    //   var node;
-
-    //   switch(selectedTool) {
-    //     case CST.TOOLS.TEXT:
-    //       node = new TextNode(evt.offsetX, evt.offsetY);
-    //       nodes.push(node);
-    //       textEditingView.setModel(node);
-    //       selectedTool = CST.TOOLS.HAND;
-    //       break;
-    //     case CST.TOOLS.HAND:
-    //       $.each(nodes, function() {
-    //         this.collide(evt.offsetX, evt.offsetY);
-    //       });
-    //       break;
-    //   }
-
-    //   refreshCanvas(canvas);
-    // })
-    // .on('mousemove', function(evt) {
-    //   $.each(nodes, function() {
-    //     if (this.dragged) {
-    //       this.moveTo(evt.offsetX, evt.offsetY);
-    //       refreshCanvas(canvas);
-    //     }
-    //   });
-    // })
-    // .on('mouseup', function(evt) {
-    //   $.each(nodes, function() { this.dragged = false; });
-    // });
   }
 
+  var toolSelection = {};
   function setupTools($tools) {
     // Text tool is selected by default
     selectedTool = CST.TOOLS.TEXT;
 
-    settings.tools.find('.text').on('click', function() {
+    toolSelection.selectText = function() {
+      $tools.find('li').removeClass('pure-menu-selected');
       selectedTool = CST.TOOLS.TEXT;
-    });
-    settings.tools.find('.hand').on('click', function() {
-      selectedTool = CST.TOOLS.HAND;
-    });
-  }
+      $tools.find('.text').addClass('pure-menu-selected');
+    };
 
-  function setCanvasStyle() {
-    var context = canvas.getContext('2d');
-    context.font = '30px Helvetica';
-    context.textBaseline = "top";
+    toolSelection.selectHand = function() {
+      $tools.find('li').removeClass('pure-menu-selected');
+      selectedTool = CST.TOOLS.HAND;
+      $tools.find('.hand').addClass('pure-menu-selected');
+    };
+
+    settings.tools.find('.text').on('click', function() {
+      toolSelection.selectText();
+    });
+
+    settings.tools.find('.hand').on('click', function() {
+      toolSelection.selectHand();
+    });
   }
 
   // Plugin
